0c10585dafed57533bdccc2ce538b46d773bb2e6
Lars Huring <lars.huring@loveatwork.se>
2015-06-02 12:58:45 +0200

    Release v0.0.2

--- gruntfile.js
+++ gruntfile.js
@@ -25,7 +25,25 @@ module.exports = function(grunt) {
                     }
                 }
             }
+        },
+
+        push: {
+            dist: {
+                options: {
+                files: ['package.json'],
+                addFiles: ['.'],
+                commit: true,
+                commitMessage: 'Release v%VERSION% deployed via Grunt push',
+                commitFiles: ['-a'], // '-a' for all files
+                createTag: false,
+                push: true,
+                pushTo: 'origin',
+                npm: false,
+                npmTag: 'Release v%VERSION% deployed via Grunt push',
+             }
+            }
         }
+
     });
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
